4ab3ec8042110d47657f24b1849b25e1a81cd3af
Cyreenbr <dallelis98@gmail.com>
2025-12-29 21:37:56 +0100

    feat : integration-test superadmin controller

--- src/main/java/com/ville/gestionincidents/config/DataInitializer.java
+++ src/main/java/com/ville/gestionincidents/config/DataInitializer.java
@@ -5,6 +5,7 @@ import com.ville.gestionincidents.enumeration.Role;
 import com.ville.gestionincidents.repository.UtilisateurRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
@@ -14,6 +15,7 @@ import org.springframework.stereotype.Component;
  */
 @Component
 @RequiredArgsConstructor
+@Profile("!test")
 public class DataInitializer implements CommandLineRunner {
 
     private final UtilisateurRepository utilisateurRepository;
